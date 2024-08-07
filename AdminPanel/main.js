50c86e7c395ad758296da707c048a29312039f58
Aria <arianaderi240@gmail.com>
2024-08-07 12:33:22 +0430

    admin panel is fully responsive !!!

--- AdminPanel/main.js
+++ AdminPanel/main.js
@@ -1,17 +1,24 @@
-// let users = document.querySelector("#users");
-// let discount = document.querySelector("#discount");
-// let product = document.querySelector("#product");
-
-// users.addEventListener("click", "showUserTable()");
-
-// const showUserTable = () => {
-//   console.log("hi");
-// };
-
 let homeBtn = document.querySelector("#home");
+let tarMenu = document.querySelector("#tra_menu");
+let crossIcon = document.querySelector("#cross");
+let body = document.querySelector("body");
+let hamberIcon = document.querySelector("#hamber_menu");
 
 const redirectTOHomePage = () => {
   location.href = "/homePage/index.html";
 };
 
 homeBtn.addEventListener("click", redirectTOHomePage);
+
+const openTraMenu = () => {
+  tarMenu.style.left = 0;
+  body.style.overflow = "hidden";
+};
+
+const closeTraMenu = () => {
+  tarMenu.style.left = -100 + "%";
+  body.style.overflow = "auto";
+};
+
+hamberIcon.addEventListener("click", openTraMenu);
+crossIcon.addEventListener("click", closeTraMenu);
