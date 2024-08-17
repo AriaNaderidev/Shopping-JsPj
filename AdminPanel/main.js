9d438c1e5cf4a080f60133afff48d227f093612c
Aria <arianaderi240@gmail.com>
2024-08-17 14:23:43 +0430

    admin panel's product adder form created and get fully responsive

--- AdminPanel/main.js
+++ AdminPanel/main.js
@@ -22,3 +22,48 @@ const closeTraMenu = () => {
 
 hamberIcon.addEventListener("click", openTraMenu);
 crossIcon.addEventListener("click", closeTraMenu);
+// end of css transition javaScript code ---------------------------------------------------------
+
+let id = document.querySelectorAll('td[data-label="id"]');
+let title = document.querySelectorAll('td[data-label="title"]');
+let price = document.querySelectorAll('td[data-label="price"]');
+let category = document.querySelectorAll('td[data-label="category"]');
+let desc = document.querySelectorAll('td[data-label="desc"]');
+let addBtn = document.querySelector("#add_btn");
+let NewRow = document.createElement("tr");
+let NewTd = document.createElement("td");
+
+const products = [
+  {
+    id: 1,
+    title: "Rolex",
+    price: "1000$",
+    discount: "0$",
+    category: "Accessory",
+    desc: "cscscsc",
+  },
+  {
+    id: 2,
+    title: "Civa",
+    price: "800$",
+    discount: "0$",
+    category: "Accessory",
+    desc: "dscsadccd",
+  },
+];
+
+products.forEach((prodcut, index) => {
+  if (products.length) {
+    id[index].textContent = prodcut.id;
+    title[index].textContent = prodcut.title;
+    price[index].textContent = prodcut.price;
+    category[index].textContent = prodcut.category;
+    desc[index].textContent = prodcut.desc;
+  }
+});
+
+const addNewProductTOTable = () => {
+  location.href = "/AdminPanel/add-product-form/index.html";
+};
+
+addBtn.addEventListener("click", addNewProductTOTable());
